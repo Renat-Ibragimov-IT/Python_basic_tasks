959f440c3ea90c391cf2f946a8896abe334391da
renat-ibragimov-it <renatibragimov26@gmail.com>
2022-07-08 17:15:12 +0300

    Next or previous task done

--- First_Steps/next_or_previous.py
+++ First_Steps/next_or_previous.py
@@ -2,12 +2,12 @@
 # the example (spaces are important!). The first line contains the next value,
 # and the second line contains the previous value of the entered number:
 # Please enter an integer number: 258
-# The next number for the number 258 is 259.
-# The previous number for the number 258 is 257.
+# The next number for the number 258 is 259
+# The previous number for the number 258 is 257
 #     or
 # Please enter an integer number: 0
-# The next number for the number 0 is 1.
-# The previous number for the number 0 is -1.
+# The next number for the number 0 is 1
+# The previous number for the number 0 is -1
 
 initial_int = int(input('Please enter an integer number: '))
 print(f'The next number for the number {initial_int} is {initial_int+1}')
