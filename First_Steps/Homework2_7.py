0d53cbd616facb05fa1f95ea8b631038a13a7acf
renat-ibragimov-it <renatibragimov26@gmail.com>
2022-07-08 17:42:38 +0300

    All tasks done

--- First_Steps/Homework2_7.py
+++
@@ -1,9 +0,0 @@
-# Написать программу "Показать звезды". На вход принимает обычное целое число,
-# должно напечатать количество звезд:
-# Пример:
-# Input number of stars: 8
-# ********
-
-print("Hi, dude!")
-asterix = int(input("Pls enter any number here: "))
-print(asterix * "*")
---
+++ First_Steps/show_stars.py
@@ -0,0 +1,8 @@
+# Write a "Show Stars" program. It takes an integer as input.
+# Should print the number of stars:
+# Example:
+# Input number of stars: 8
+# ********
+
+asterix = int(input("Please enter any number here: "))
+print(asterix * "*")
